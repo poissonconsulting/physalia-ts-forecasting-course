56b8ffbec465b9669bbc54ee3771bed5b6cbc514
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-01-16 14:17:36 -0800

    added other packages for day 1

--- packages.R
+++ packages.R
@@ -3,10 +3,13 @@ if(FALSE) {
 }
 
 # attach necessary packages
+library('ctmm')      #' for continuous-time stochastic models
 library('dplyr')     #' for data wrangling and tidying
 library('tidyr')     #' for data wrangling and tidying
+library('purrr')     #' for functional programming
 library('lubridate') #' for working with dates
 library('ggplot2')   #' for fancy plots
 library('mvgam')     #' for fitting models; uses `mgcv`, `brms` and `cmdstan`
+library('khroma')    #' for color palettes
 
 theme_set(theme_bw() + theme(text = element_text(face = 'bold')))
