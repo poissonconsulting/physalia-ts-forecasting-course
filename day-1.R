a7c860e52c4df834fa14d40b44f28413c016ee4f
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-01-23 08:39:58 -0800

    cleaned up comments and plots

--- day-1.R
+++ day-1.R
@@ -113,7 +113,7 @@ d_ts <- tibble(t = 1:1e3,
 for(i in 2:nrow(d_ts)) {
   d_ts$ar_1[i] <- d_ts$w[i] + d_ts$ar_1[i - 1] * 0.9
   d_ts$ma_1[i] <- d_ts$w[i] + d_ts$w[i - 1] * 0.9
-  d_ts$arma_11[i] <- d_ts$w[i] + d_ts$ar_1[i - 1] * 0.9 + d_ts$w[i - 1] * 0.9
+  d_ts$arma_11[i] <- d_ts$w[i] + d_ts$ar_1[i - 1] * 0.9 + d_ts$w[i - 1] * (-0.5)
   rm(i)
 }
 
@@ -122,12 +122,12 @@ ggplot(d_ts, aes(t, w)) +
   geom_hline(yintercept = 0, lty = 'dashed') +
   geom_line()
 
-layout(t(1:2))
+layout(1:2)
 acf(d_ts$w, ci = 0.99) # first lag-0 AR will always be at 1
 pacf(d_ts$w, ci = 0.99) # no appreciable partial correlations
 arima(d_ts$w, order = c(0, 0, 0)) %>% coef()
 
-#' *AR(1) process*: `y_t` is a weighted average of of `y_{t-i}` and `w_t`
+#' *AR(1) process*: `y_t` is a function of of `y_{t-i}` and `w_t`
 ggplot(d_ts, aes(t, ar_1)) +
   geom_hline(yintercept = 0, lty = 'dashed') +
   geom_line()
@@ -137,7 +137,7 @@ m_ar <- arima(d_ts$ar_1, order = c(1, 0, 0))
 coef(m_ar)
 acf(resid(m_ar)); pacf(resid(m_ar)) # residuals don't have appreciable signals
 
-#' *MA(1) process*: errors of `y_t` are a weighted average of `w_t` and `w_{t-1}`
+#' *MA(1) process*: errors of `y_t` are a function of `w_t` and `w_{t-1}`
 ggplot(d_ts, aes(t, ma_1)) +
   geom_hline(yintercept = 0, lty = 'dashed') +
   geom_line()
@@ -152,8 +152,8 @@ ggplot(d_ts, aes(t, arma_11)) +
   geom_hline(yintercept = 0, lty = 'dashed') +
   geom_line()
 
-acf(d_ts$arma_11, ci = 0.99); pacf(d_ts$arma_11, ci = 0.99)
-m_arma <- arima(d_ts$ma_1, order = c(1, 0, 1))
+acf(d_ts$arma_11, ci = 0.99); pacf(d_ts$arma_11, ci = 0.99) # AR + MA plots
+m_arma <- arima(d_ts$arma_11, order = c(1, 0, 1))
 coef(m_arma)
 acf(resid(m_arma)); pacf(resid(m_arma))
 layout(1)
@@ -190,50 +190,64 @@ d_ts %>%
                                 model_type == 'ma_1' ~ 'MA(1)',
                                 model_type == 'arma_11' ~ 'ARMA(1, 1)',
                                 model_type == 'w' ~ 'White noise')) %>%
+  mutate(int = mean(value), .by = model_type) %>%
   ggplot() +
   facet_wrap(~ model_type) +
-  geom_hline(aes(yintercept = mean(value)), lty = 'dashed') +
+  geom_hline(aes(yintercept = int), lty = 'dashed') +
   geom_point(aes(t, value), alpha = 0.5) +
   geom_ribbon(aes(t, ymin = lwr_90, ymax = upr_90), preds, alpha = 0.3,
               lwd = 0.5, , fill = 'darkorange', color = 'darkorange') +
   geom_line(aes(t, pred), preds, color = 'darkorange', lwd = 1) +
   labs(x = 'Timepoint', y = 'Value')
 
-#' see `?AirPassengers` for an example that includes seasonal trends
+#' see `?AirPassengers` for an example model that includes seasonal trends
 
 ##' *NOTE:* processes are stationary if `sum(coefs)` < 1 (not intercept)
-plot_process <- function(ar = c(0), ma = c(0), return_values = FALSE) {
-  out <- rep(NA_real_, nrow(d_ts)) # vector of values to return
-  w <- d_ts$w
+plot_process <- function(ar = c(0), ma = c(0), n = 1e3, return_values = FALSE) {
+  out <- rep(NA_real_, n) # vector of values to return
+  w <- rnorm(n)
   
-  for(i in 1:nrow(d_ts)) {
-    ## create indices to reference the previous observations
-    ar_indices <- (i - length(ar)):(i - 1)
-    ma_indices <- (i - length(ma)):(i - 1)
+  for(i in 1:n) {
+    ##' take `length(coefs)` previous observations
+    ar_indices <- i - (length(ar):1)
+    ma_indices <- i - (length(ma):1)
     
     ##' negative and `integer(0)` indices cause issues
     ar_indices[ar_indices <= 0] <- NA_integer_
     ma_indices[ma_indices <= 0] <- NA_integer_
     
-    out[i] <- sum(w[i], sum(out[ar_indices] * ar), sum(w[ma_indices] * ma),
+    ##' need to use `sum()` to drop the `NA`s
+    out[i] <- sum(w[i], out[ar_indices] * ar, w[ma_indices] * ma,
                   na.rm = TRUE)
   }
   
+  main_lab <- case_when(all(ar == 0, ma == 0) ~ 'White noise',
+                        all(ar != 0 & ma == 0) ~ 'AR process',
+                        all(ar == 0 & ma != 0) ~ 'MA process',
+                        all(ar != 0 & ma != 0) ~ 'ARMA process',
+                        .default = 'Odd parameter input!')
+  
+  if(round(sum(ar, na.rm = TRUE), 10) >= 1) { # to rm floating point error
+    main_lab <- paste(main_lab, '(non-stationary)')
+  }
+  
   layout(matrix(c(1, 1, 2, 3), ncol = 2, byrow = TRUE))
-  plot(out, type = 'l')
-  acf(out, main = NULL)
-  pacf(out, main = NULL)
+  plot(out, type = 'l', ylab = 'Value',
+       main = main_lab)
+  acf(out, main = '')
+  pacf(out, main = '')
   layout(1)
   if(return_values) return(out)
 }
 
 plot_process(ar = 0, ma = 0) # white noise
 
-# AR processes: stationary if coefficients sum to <= 1
+# AR processes: stationary if coefficients sum to < 1
 plot_process(ar = c(0.7, 0.2)) # stationary since 0.7 + 0.2 < 1
 plot_process(ar = c(0.7, 0.6)) # non-stationary since 0.7 + 0.6 > 1
 plot_process(ar = c(0.5, 0.4, 0.3)) # non-stationary since sum > 1
-plot_process(ar = c(0.4, 0.3, 0.2, 0.1)) # stationary since sum = 1
+plot_process(ar = c(0.4, 0.3, 0.2, 0.0999)) # stationary since sum < 1
+plot_process(ar = c(0.4, 0.3, 0.2, 0.0999), n = 1e5) # stationary since sum < 1
 
 # MA processes are always stationary: coefficients only scale the variance
 plot_process(ma = c(0.9, 0.1))
@@ -242,6 +256,7 @@ plot_process(ma = c(700, 50))
 
 # ARMA processes: stationary if AR is stationary
 plot_process(ar = c(0.7, 0.2), ma = c(0.5, 0.3)) # stationary ARMA(2, 2)
+plot_process(ar = c(0.7, 0.3), ma = c(0.5, 0.3)) # non-stationary ARMA(2, 2)
 
 ## applying the plots to the air temperature series ----
 layout(t(1:2))
@@ -254,6 +269,7 @@ pacf(d_temp$temp) #' correl. between pairs, without previous lags' effects
 m_ar_temp <- arima(d_temp$temp, order = c(1, 0, 0))
 coef(m_ar_temp)
 acf(resid(m_ar_temp)); pacf(resid(m_ar_temp)) # residuals are ok
+layout(1)
 
 ## strengths and limitations of ARMA models:
 ## - because coefficient estimates depend on pairs of points, the models are
@@ -300,7 +316,8 @@ data.frame(track) %>%
          time_interval = t - lag(t),
          speed = displacement / time_interval) %>%
   ggplot(aes(t, speed)) +
-  geom_line()
+  geom_line() +
+  ylab('SLD divided by time interval (m/s)')
 
 # calculate SLD for thinned time series
 d_track <- tibble(thinning = 1:60,
@@ -318,10 +335,22 @@ d_track <- tibble(thinning = 1:60,
                   }))
 d_track
 
+## plot the estimated speed for each sampling interval
+d_track %>%
+  unnest(data) %>%
+  filter(sampling_interval <= 540) %>%
+  mutate(sampling_interval = paste(sampling_interval, 'seconds') %>%
+           factor(., unique(.))) %>%
+  ggplot(aes(t, speed)) +
+  facet_wrap(~ sampling_interval) +
+  geom_line() +
+  ylab('SLD divided by time interval (m/s)')
+
 ## estimated speed decreases substantially with sampling interval...
 ggplot(d_track, aes(sampling_interval, mean_speed)) +
   geom_line() +
-  labs(x = 'Sampling interval (seconds)', y = 'Mean speed (m/s)')
+  labs(x = 'Sampling interval (seconds)',
+       y = 'Straight-line displacement divided by time interval (m/s)')
 
 ## ... which is not surprising since we are losing data on the complexity of the
 ## tracks as we thin them. the issues compound if sampling intervals are
@@ -331,7 +360,9 @@ ggplot(d_track, aes(sampling_interval, mean_speed)) +
 ## - https://doi.org/10.1101/2025.07.17.665364
 d_track %>%
   filter(thinning %in% c(1, 5, 10, 15, 30, 60)) %>%
-  mutate(sampling_interval = paste0(sampling_interval / 60, ' (minutes)') %>%
+  mutate(sampling_interval =
+           paste(sampling_interval / 60,
+                 if_else(sampling_interval == 60, 'minute', 'minutes')) %>%
            factor(., levels = unique(.))) %>%
   unnest(data) %>%
   ggplot(aes(x, y)) +
@@ -381,7 +412,7 @@ d_track %>%
 #' `?brms::family.brmsfit` for families supported by `{mgcv}` or `{brms}` but
 #' not necessarily supported by `{mvgam}`.
 
-##' *choose a link function based on the support of the distribution*
+##' *choose a link function based on the possible values for the distribution*
 ##' unbounded: identity; `I(-Inf, Inf) = (-Inf, Inf)`
 ##' `Y >= 0` or `Y > 0`: `log(0, Inf) = (-Inf, Inf)`
 ##' `0 <= Y <= 1`: `logit(0, 1) = log(odds(0, 1)) = log(0,Inf) = (-Inf,Inf)`
@@ -395,8 +426,9 @@ d_track %>%
 
 ##' `E(Y) = mu`
 ##' `g(mu) = eta = b_0 + b_1 * x_1 + b_2 * x_2`
+##' `mu = g^{-1}(eta) = g^{-1}(b_0 + b_1 * x_1 + b_2 * x_2)`
 
-## how can we model data with irregular sampling over time?
+## how can we model data that has irregular sampling over time?
 ##' *NOTE:* many of the `{mvgam}` plots assume discrete-time sampling, so the
 ##' missing observations should be `NA` rather than missing the full row, as
 ##' long as none of the predictors have `NA` values.
@@ -431,8 +463,13 @@ m_temp_poly <- mvgam(temp ~ doy_1 + doy_2 + doy_3,
 
 ##' since `{mvgam}` fits Bayesian models with `Stan`, we should check that all
 ##' chains converged properly: check `Rhat`, `n_eff`, and Stan MCMC diagnostics
+##' each chain is one of the "paths" the model took to estimate the parameters
+##' the model will spend more time near the better parameter estimates and less
+##' near unlikely values, which estimates the posterior distribution.
+##' ideally, all chains should be similar and undistiguishable.
 summary(m_temp_poly)
-plot(m_temp_poly, type = 'residuals') #' model diagnostics `plot_mvgam_resids()`
+mcmc_plot(m_temp_poly, type = 'trace', variable = rownames(coef(m_temp_poly)))
+plot(m_temp_poly, type = 'residuals') #' can also use `plot_mvgam_resids()`
 
 ## can add predictions to the data...
 d_temp_missing %>%
@@ -464,8 +501,14 @@ m_temp_gam <- mvgam(temp ~ s(doy, k = 10, bs = 'cr'),
 ##' - `s(doy)` has `k - 1` coefficients
 ##' - each coefficient is multiplied by the respective basis function
 summary(m_temp_gam)
-coef(m_temp_gam$mgcv_model) # model coeffients
-coef(m_temp_gam$mgcv_model)[-1] # drop intercept term
+coef(m_temp_gam$mgcv_model) # model coefficients
+coef(m_temp_gam$mgcv_model)[-1] #' check `s(doy)` terms only
+mcmc_plot(m_temp_gam, type = 'trace',
+          variable = c('sigma_obs[1]', 's(doy)_rho', 's(doy)._rho'),
+          regex = TRUE)
+
+#' `mcmc_plot()` only plots intercept and rho terms by default
+mcmc_plot(m_temp_gam, type = 'trace', variable = c('.'), regex = TRUE)
 
 #' visualize the cubic basis
 draw(basis(s(doy, bs = 'cr'), data = d_temp_missing)) # default cubic basis
@@ -478,9 +521,10 @@ draw(basis(m_temp_gam$mgcv_model)) & # fitted cubic basis
   geom_line(aes(doy, .fitted - coef(m_temp_gam$mgcv_model)[1]),
             fitted_values(m_temp_gam$mgcv_model), lwd = 1,
             inherit.aes = FALSE)
+
 plot(hindcast(m_temp_gam, type = 'response')) # uncertainty in Y
 plot(hindcast(m_temp_gam, type = 'link')) # uncertainty in mu on link scale
-plot(hindcast(m_temp_gam, type = 'expected')) # uncertainty in mu
+plot(hindcast(m_temp_gam, type = 'expected')) # identity link: same as above
 
 ##' smooths are centered at 0
 plot(m_temp_gam, type = 'smooths') #' model terms; == `plot_mvgam_smooth()`
@@ -496,7 +540,7 @@ ggplot(d_temp_missing) + geom_point(aes(week_re, temp), alpha = 0.3)
 m_temp_re <- mvgam(temp ~ s(week_re, bs = 're'), family = gaussian(),
                    data = d_temp_missing, parallel = TRUE, silent = 2)
 summary(m_temp_re)
-draw(m_temp_re$mgcv_model)
+draw(m_temp_re$mgcv_model) # each week has a coefficient
 
 plot(hindcast(m_temp_re, type = 'response')) # response scale; uncertainty in Y
 
@@ -531,12 +575,11 @@ m_temp_ar <- mvgam(formula = temp ~ 0,
                    trend_formula = ~ s(doy, k = 10, bs = 'cr'),
                    trend_model = AR(1),
                    family = gaussian(), data = d_temp_missing, noncentred = TRUE,
-                   parallel = TRUE, burnin = 500, samples = 1e4,
+                   parallel = TRUE, burnin = 500, samples = 1000,
                    control = list(max_treedepth = 20, adapt_delta = 0.99))
 
-summary(m_temp_ar) # chains are occasionally not well-mixed
+# chains are occasionally not well-mixed even if Rhat is near 1
+mcmc_plot(m_temp_ar, type = 'trace')
+summary(m_temp_ar)
 plot(m_temp_ar) # no appreciable correlations at any lags
-
-# caterpillar plot: inspect traces of paths for estimating the parameters
-mcmc_plot(m_temp_ar, type = 'trace') +
-  theme(legend.position = 'inside', legend.position.inside = c(5/6, 1/4))
+# uncertainty at greater lags is because of missing data
