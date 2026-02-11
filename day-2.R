e92e245ddb3ad8949a77912b9c55f1b8e64455f0
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-02-11 07:11:49 -0800

    cleaned up and added comments

--- day-2.R
+++ day-2.R
@@ -48,7 +48,7 @@ data_train <- filter(air_passengers, year <= 1958)
 data_test <- filter(air_passengers, year > 1958)
 
 ##' fit a simple gam with `dec_date`
-##' *NOTE:* fitting times are a lot slower if passengers is multiplied by 1000
+##' *NOTE:* fitting times are a lot slower if `passengers` is multiplied by 1000
 m_gam <- mvgam(formula = passengers ~ s(dec_date, k = 30),
                family = poisson(),
                data = data_train,
@@ -87,12 +87,15 @@ m_gam_smooth <- mvgam(formula = passengers ~ s(dec_date, k = 10),
 # predictions are better, but the model is missing the seasonality
 plot(hindcast(m_gam_smooth))
 plot(m_gam_smooth, type = 'forecast')
+draw(basis(m_gam_smooth$mgcv_model)) # simpler bases
 
 ##' we can improve the predictions somewhat by extending the basis, but this
 ##' still depends on the model and the complexity of the trends...
-##' for more info, see `https://fromthebottomoftheheap.net/2020/06/03/extrapolating-with-gams/`
+##' for more info, see:
+##' `https://fromthebottomoftheheap.net/2020/06/03/extrapolating-with-gams/`
 ##' fit a GAM with a cubic B spline whose curvature and slope are penalized
-m_gam_bs <- mvgam(formula = passengers ~ s(dec_date, k = 30, bs = 'bs',
+##' note that `k` is quite high now
+m_gam_bs <- mvgam(formula = passengers ~ s(dec_date, k = 50, bs = 'bs',
                                            m = c(3, 2, 1)),
                   knots = list(dec_date = c(min(data_train$dec_date),
                                             max(data_test$dec_date))),
@@ -105,19 +108,17 @@ m_gam_bs <- mvgam(formula = passengers ~ s(dec_date, k = 30, bs = 'bs',
                   control = list(max_treedepth = 20, adapt_delta = 0.9),
                   parallel = TRUE)
 
-##' warnings indicate there is no data for a portion of the spline (as expected)
+##' warnings indicate there are no data for a portion of the spline, as expected
 draw(basis(m_gam_bs$mgcv_model, data = air_passengers)) +  # model bases
   geom_rug(aes(dec_date), data_train, inherit.aes = FALSE) # rug plot of data
 
-# predictions for training data aren't as good now
-plot(hindcast(m_gam_bs))
-
-## predictions for the test data are better, but still not good
+## forecasts for the test data are better, but still not good
 ## the model doesn't know there are seasonal cycles. it only understands that
-## the values go up and down, but not why they do.
-layout(1:2)
+## the values go up and down, but not *why* they do.
+layout(1:3)
 plot(m_gam, type = 'forecast')
-plot(m_gam_bs, type = 'forecast') ## lower DRPS implies better forecasts
+plot(m_gam_smooth, type = 'forecast') ## lower DRPS implies better forecasts
+plot(m_gam_bs, type = 'forecast')
 layout(1)
 
 ##' more rigid models tend to extrapolate better, so we can decompose the trend
@@ -148,7 +149,7 @@ layout(matrix(1:4, ncol = 2))
 plot(m_gam, type = 'forecast')
 plot(m_gam_smooth, type = 'forecast')
 plot(m_gam_bs, type = 'forecast')
-plot(m_gam_month, type = 'forecast') # best model, but over-predicts a bit
+plot(m_gam_month, type = 'forecast') # best model, but under-predicts a bit
 layout(1)
 
 ## assuming the seasonal cycle repeats across years allows us to reduce the
@@ -157,15 +158,15 @@ layout(1)
 # autocorrelation present at lags 1 and 12
 plot(m_gam_month)
 
-## Auger-Méthé et al. (2021; https://doi.org/10.1002/ecm.1470):
-## The assumptions that the hidden states are autocorrelated (e.g., that a large
-## population in year t will likely lead to a large population in year t + 1),
-## and that observations are independent once we account for their dependence on
-## the states (Fig. 1a), allow SSMs to separate these two levels of
-## stochasticity.
+## SSMs separate true trend from obervation error by assuming that
+## (1) true values of the response are autocorrelated (e.g., y_t will likely be
+## similar to y_{t-1}), and
+## (2) residuals are independent once we account for the model (including
+## previous values of y)
+## see Auger-Méthé et al. (2021; https://doi.org/10.1002/ecm.1470) for more info
 
 # fit a GAM with an AR(1) process
-m_gam_ar <- mvgam(formula = passengers ~ 0, # assuming no error in response
+m_gam_ar <- mvgam(formula = passengers ~ 0, # no error in observation process
                   trend_formula = ~
                     s(year, k = 9, bs = 'tp') +
                     s(month, k = 10, bs = 'cc'),
@@ -178,13 +179,14 @@ m_gam_ar <- mvgam(formula = passengers ~ 0, # assuming no error in response
                   burnin = 750,
                   samples = 500,
                   control = list(max_treedepth = 20, adapt_delta = 0.95),
-                  parallel = TRUE, silent = 2)
+                  parallel = TRUE)
 
 ##' the `m_gam_ar` model is:
-##' `passengers ∼ Poisson(λ_t)`
-##' `log(λ_t) = b_0 + s(year) + s(month) + z_t`
-##' `z_t ∼ Normal(z_{t−1} * a, σ)`
-##' `σ ∼ Exponential(2)`
+##' `passengers ∼ Poisson(λ_t)`           # observation
+##' `log(λ_t) ~ Normal(mu, S)`            # mean observation on log scale
+##' `mu = b_0 + s(year) + s(month) + z_t` # latent trend on log scale
+##' `z_t ∼ Normal(z_{t−1} * a, σ)`        # latent error variable
+##' `σ ∼ Exponential(2)`                  # SD of latent error variable
 ##' where `a` is the coefficient of the `AR(1)` process
 
 summary(m_gam_ar) # diagnostics are ok
@@ -193,10 +195,10 @@ summary(m_gam_ar) # diagnostics are ok
 # intervals are 50%, 60%, and 90%, estimated from samples
 plot(m_gam_ar)
 
-#' *NOTE:* we need to drop rows with `NA` predictors because the model can't use
-#' them. specifying the AR process in the `trends_model` argument avoids this
-#' issue. This is why we shouldn't just add the lagged values as predictors in a
-#' GAM.
+#' *NOTE:* we need to drop rows with `NA` lagged y values because the model
+#' can't use them. specifying the AR process in the `trends_model` argument
+#' avoids this issue. This is why we shouldn't just add the lagged values as
+#' predictors in a GAM.
 data_train_12 <- mutate(data_train, lag_12_passengers = lag(passengers, 12)) %>%
   filter(! is.na(lag_12_passengers))
 data_test_12 <- air_passengers %>%
@@ -204,6 +206,7 @@ data_test_12 <- air_passengers %>%
   filter(year > 1958, ! is.na(lag_12_passengers))
 
 # add a term to crudely account for a 12-month autocorrelation
+# fits in ~100 seconds
 m_gam_ar_12 <- mvgam(formula = passengers ~ 0,
                      trend_formula = ~
                        lag_12_passengers +
@@ -223,6 +226,8 @@ m_gam_ar_12 <- mvgam(formula = passengers ~ 0,
 coef(m_gam_ar_12$trend_mgcv_model)['lag_12_passengers']
 plot(m_gam_ar) # see values at lag-12 for ACF and pACF
 plot(m_gam_ar_12) # values at lag-12 are smaller, but lag-1 values are larger
+plot_grid(plot(m_gam_ar), # see values at lag 12 for ACF and pACF
+          plot(m_gam_ar_12)) # values at lag 12 are smaller, but lag 1 is larger
 
 ##' how does `{mvgam}` handle many missing data?
 ##' `{mvgam}` does not drop rows with `NA` response values, so it keeps track of
