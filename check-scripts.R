a1b6f7820edfee8334ca4beef44d82c62a7c838e
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-03-19 14:25:14 -0700

    clean up checking script

--- check-scripts.R
+++ check-scripts.R
@@ -1,11 +1,13 @@
 # check for tasks to do
-findR::findRscript(pattern = 'TODO', path = '.', case.sensitive = FALSE,
-                   comments = TRUE)
+findR::findRscript(pattern = "TODO", path = ".", case.sensitive = FALSE,
+                   comments = TRUE) %>%
+  filter(path_to_file != "./check-scripts.R")
 
 # check for place markers
-findR::findRscript(pattern = 'HERE', path = '.', case.sensitive = TRUE,
-                   comments = TRUE)
+findR::findRscript(pattern = "HERE", path = ".", case.sensitive = TRUE,
+                   comments = TRUE) %>%
+  filter(path_to_file != "./check-scripts.R")
 
-findR::findRscript(pattern = 'DELETE', path = '.', case.sensitive = TRUE,
-                   comments = TRUE)
-
+findR::findRscript(pattern = "DELETE", path = ".", case.sensitive = TRUE,
+                   comments = TRUE) %>%
+  filter(path_to_file != "./check-scripts.R")
