100f33d04cdb6a28c22246920fd1cc70d3dfc5cd
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-04-14 13:04:58 -0700

    add tourism dataset to day 5

--- day-5.R
+++ day-5.R
@@ -1,9 +1,11 @@
 source('packages.R') # attach necessary packages
+library('timeSeriesDataSets') # for datasets
+library('tsibble') #' to read in quarters correctly for `tourism_tbl_ts` below
 
 # Extended practical examples using {mvgam}
 #' more good datasets included in `good-datasets.R`
 
-# a dataset of pedestrian counts
+# hourly pedestrian counts ----
 ped <- timeSeriesDataSets::pedestrian_tbl_ts %>%
   rename_with(tolower) %>%
   mutate(date = as_date(date_time),
@@ -39,3 +41,16 @@ ped %>%
   ggplot(aes(h, count, group = date)) +
   facet_wrap(~ sensor) +
   geom_line(alpha = 0.05)
+
+# daily Australian domestic overnight trips ----
+tourism_tbl_ts
+
+layout(1:2)
+acf(filter(tourism_tbl_ts, Region == Region[1])$Trips)
+pacf(filter(tourism_tbl_ts, Region == Region[1])$Trips)
+layout(1)
+
+ggplot(tourism_tbl_ts, aes(as.Date(Quarter), Trips, group = Region, color = Purpose)) +
+  facet_grid(State ~ Purpose) +
+  geom_line() +
+  scale_color_bright()
